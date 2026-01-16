13b80353e4e912ae2ec1e489f0d55292c24433d5
siddbytheway <88389185+SiddharthIsCoding@users.noreply.github.com>
2026-01-16 17:04:27 +0530

    connected

--- main.py
+++ main.py
@@ -1,2 +1,56 @@
-import mysql.connector , random , os 
+import mysql.connector , random , os , time
+from colorama import *
+
+init(autoreset=True)
+
+os.system("cls")
+
+loggedin = False
+
+
+
+
+
+
+try: 
+
+    myconn = mysql.connector.connect(host="localhost",
+                                     username="root",
+                                     passwd="root",
+                                     db="project0")
+    
+    if myconn:
+        print('\n'+Fore.GREEN+Back.BLUE+'\tCONNECTED')
+    
+    mycur = myconn.cursor()
+
+
+    def login():
+       os.system('cls')
+       print( "\n\n%50s"%""+ Back.GREEN + Fore.LIGHTGREEN_EX + " LOGIN ")
+
+       print( "\n%45s"%""+ Fore.GREEN + "Enter username ")
+       username = input("%40s"%""+ Fore.GREEN+ ">>> ")
+    
+       print( "\n%45s"%""+ Fore.GREEN + "Enter password")
+       password = input( "%40s"%""+Fore.GREEN+">>> ")
+
+       mycur.execute("INSERT INTO users VALUES(%d,%s,%d)"%(random.randrange(99,1000),username,password))
+       print("Loaded")
+
+
+    login()
+
+
+   
+
+
+except:
+
+    print("\n\n\n" + Fore.RED + Back.GREEN + "CONNECTION FAILED")
+
+
+
+
+
 
